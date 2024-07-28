c8d8622adce8282cbbb135233bfb79190cc02c64
Aditya Ranjan <adityaranjan_2004@outlook.com>
2024-07-28 16:33:32 +0530

    Made inputs integers

--- Web_App.py
+++ Web_App.py
@@ -17,15 +17,14 @@ st.set_page_config(
     menu_items={
         'Report a bug': "https://github.com/Defalt-here/FLAMES-couple-predictor/issues",
         'About': "Go ahead check if you're compatible with your crush!\
-        Training dataset found at: https://www.kaggle.com/datasets/mexwell/speed-dating/data\
-        Check me out on github at: https://github.com/Defalt-here\
-        "
+        Training dataset found at: https://www.kaggle.com/datasets/mexwell/speed-dating/data"
+
     }
 )
 def main():
     st.title("FLAMES")
     st.subheader("Check out your compatibility with your crush, or your partner maybe you'll fall in love maybe you'll breakup who knows?")
-    st.text("Put all the required inputs in integers(no decimals) between 0 to 10 both inclusive")
+    st.text("To get the training data for yourself check the about section! For more information check out my github on the top right")
     # gender, age, income, attraction, sincerity, intelligence, funny, ambition, interests, overall, reciprocate, met
     option = st.selectbox(
     "Select your gender?",
@@ -34,15 +33,6 @@ def main():
         gender = 1
     else:
         gender = 0
-    age = st.number_input("Enter your age")
-    attraction = st.number_input("How attracted you are to the person")
-    sincerity = st.number_input("How sincere is the person in question?")
-    intelligence = st.number_input("How intelligent is the person in question")
-    funny = st.number_input("How funny is the person in question")
-    ambition = st.number_input("How ambitious is the person in question")
-    interests = st.number_input("How many shared interests do you have with the person?")
-    overall = st.number_input("Put an overall score for the person")
-    reciprocate = st.number_input("Do you think the person will reciprocate your emotions?")
     met_option = st.selectbox(
     "Have you met before?",
     ("Met before", "Not met"))
@@ -50,10 +40,18 @@ def main():
         met = 1
     else:
         met = 2
+    age = st.number_input("Enter your age",min_value=0,max_value=10,step=1)
+    attraction = st.number_input("How attracted you are to the person",min_value=0,max_value=10,step=1)
+    sincerity = st.number_input("How sincere is the person in question?",min_value=0,max_value=10,step=1)
+    intelligence = st.number_input("How intelligent is the person in question",min_value=0,max_value=10,step=1)
+    funny = st.number_input("How funny is the person in question",min_value=0,max_value=10,step=1)
+    ambition = st.number_input("How ambitious is the person in question",min_value=0,max_value=10,step=1)
+    interests = st.number_input("How many shared interests do you have with the person?",min_value=0,max_value=10,step=1)
+    overall = st.number_input("Put an overall score for the person",min_value=0,max_value=10,step=1)
+    reciprocate = st.number_input("Do you think the person will reciprocate your emotions?",min_value=0,max_value=10,step=1)
     rating = "Are you compatible? 🥁"
     if st.button("Enter"):
         rating = Prediction([gender, age, attraction, sincerity, intelligence, funny, ambition, interests, overall, reciprocate, met])
     st.success(rating)
-    
 if __name__ == "__main__":
     main()
