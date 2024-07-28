b53f202464708948e08aae6b418c4b89488eab59
Aditya Ranjan <adityaranjan_2004@outlook.com>
2024-07-28 14:23:45 +0530

    .

--- Web_App.py
+++ Web_App.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pickle
 import streamlit as st
+import sklearn
 loaded_model = pickle.load(open("newModel.sav","rb"))
 def Prediction(input_data):
     npArray = np.array(input_data)
